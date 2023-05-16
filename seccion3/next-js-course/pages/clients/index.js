503111e1591aad612d51baeb3c0e905749c4434c
German Bertea <gebe@influencerindex.com>
2023-05-16 22:41:01 +0200

    move with links for move across pages, use dinamic paths

--- seccion3/next-js-course/pages/clients/index.js
+++ seccion3/next-js-course/pages/clients/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import Link from 'next/link'
 
 const ClientsPage = () => {
+
+  const clients = [
+    { id: '1', name: 'ger' },
+    { id: '2', name: 'max' },
+    { id: '3', name: 'nen' }
+  ]
   return <div>
     <h1>The Default clients page</h1>
+    <ul>
+      {clients.map(client =>
+        <li key={client.id}>
+          <Link href={`/clients/${client.id}`}>{client.name}</Link>
+        </li>
+      )}
+    </ul>
   </div>
 }
 
--- seccion3/next-js-course/pages/index.js
+++ seccion3/next-js-course/pages/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import Link from 'next/link'
 
 const HomePage = () => {
   return <div>
     <h1>The HomePage</h1>
+    <ul>
+      <li>
+        <Link  href="/portfolio">portfolio</Link>
+      </li>
+      <li>
+        <Link href="/clients">clients</Link>
+      </li>
+      <li>
+        <Link href="/about">about</Link>
+      </li>
+    </ul>
   </div>
 }
 
