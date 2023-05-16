62fa70e8f5c939d5be5638946015abefa682ee10
German Bertea <gebe@influencerindex.com>
2023-05-16 22:51:59 +0200

    add programatically redirect

--- seccion3/next-js-course/pages/clients/[id]/index.js
+++ seccion3/next-js-course/pages/clients/[id]/index.js
@@ -6,8 +6,25 @@ const ClientProjectsPage = () => {
   const router = useRouter()
 
   console.log(router.query)
+
+
+  const loadProjectHandler = () => {
+
+    // router.push('/clients/ger/project a') // we move to another page programatically
+    // router.replace('/clients/ger/project a')// we remplace with another page programatically( we cant comeback)
+    router.push({
+      pathname:'/clients/[id]/[clientprojectid]',
+      query: {
+        id:'ger',
+        clientprojectid :'project a'
+      }
+    })
+  }
+
+
   return <div>
     <h1>The project of a given client</h1>
+    <button onClick={loadProjectHandler}> load project a</button>
   </div>
 }
 
