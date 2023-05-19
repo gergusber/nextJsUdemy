47023768180f20cdf6574b433aebc508558db6fe
German Bertea <gebe@influencerindex.com>
2023-05-19 16:48:11 +0200

    work in different ways to get the data form dummy backend

--- seccion5-data-fetching/seccion5-js/pages/index.js
+++ seccion5-data-fetching/seccion5-js/pages/index.js
@@ -1,11 +1,36 @@
+// import data from '../data/dummy-backend.json'
+import path from 'path'
+import fs from 'fs/promises'
+
+
 function HomePage(props) {
+  const { products } = props
   return (
     <ul>
-      <li>Product 1</li>
+      {products.map(x => { return <li key={x.id}>{x.title}</li> })}
+      {/* 
       <li>Product 2</li>
       <li>Product 3</li>
+      <li>{ }</li> */}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filepath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filepath)
+  const data = JSON.parse(jsonData)
+  return {
+    // props: data
+    props: {
+      // products: [
+      //   { id: 'p1', title: 'product1' },
+      //   { id: 'p2', title: 'product2' },
+      //   { id: 'p3', title: 'product3' },
+      // ]
+      ...data
+    }
+  };
 }
 
 export default HomePage;
