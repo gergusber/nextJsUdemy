e7167de43390bc824c814967954a7900d6f306a4
German Bertea <gebe@influencerindex.com>
2023-05-21 18:05:50 +0200

    use swr for data fetching

--- seccion5-data-fetching/seccion5-js/pages/last-sales.js
+++ seccion5-data-fetching/seccion5-js/pages/last-sales.js
@@ -1,43 +1,67 @@
 import { useEffect, useState } from "react";
-
-
+import useSWR from 'swr'
+const URL_API_FIREBASE = 'url';
 
 const LastSalesPage = (props) => {
-
+  //useSWR(<request-url>, (url) => fetch(url).then(res => res.json())) if problems
+  const { data, error } = useSWR(URL_API_FIREBASE, (url) => fetch(url).then(res => res.json())); //seSWR(URL_API_FIREBASE);
+ 
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    setIsLoading(true)
-    const sales = fetch(
-      'firebase url'
-    ).then(response => response.json())
-      .then(data => {
-        const transformedSaleS = [];
+    const transformedSaleS = [];
 
-        for (const key in data) {
-          transformedSaleS.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume
-          })
-        }
+    if (data) {
 
-        setSales(transformedSaleS)
-        setIsLoading(false)
-      });
-  }, [])
+      for (const key in data) {
+        transformedSaleS.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume
+        })
+      }
+    }
+    setSales(transformedSaleS)
+  }, [data])
 
-  if (isLoading) {
+
+  // const [sales, setSales] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+  // useEffect(() => {
+  //   setIsLoading(true)
+  //   const sales = fetch(URL_API_FIREBASE).then(response => response.json())
+  //     .then(data => {
+  //       const transformedSaleS = [];
+
+  //       for (const key in data) {
+  //         transformedSaleS.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume
+  //         })
+  //       }
+
+  //       setSales(transformedSaleS)
+  //       setIsLoading(false)
+  //     });
+  // }, [])
+  if (error)
+    return <p> Fail to load...</p>
+
+
+  if (!data)
     return <p> Loading...</p>
+
+  if (!sales)
+    return <p> Loading...</p>
+  else{
+    return (<ul>
+      {sales ? sales.map(sale => <li key={sale.id}>
+        {sale.username} - ${sale.volume}
+      </li>) : null}
+    </ul>)
   }
-  if (!sales) {
-    return <p> fetching...</p>
-  }
-  return (<ul>
-    {sales ? sales.map(sale => <li key={sale.id}>
-      {sale.username} - ${sale.volume}
-    </li>) : null}
-  </ul>)
+
 }
 
 export default LastSalesPage;
