c086eea47c7c086e68c1fbac6c6cc1d195bdae8d
German Bertea <gebe@influencerindex.com>
2023-05-23 21:06:43 +0200

    add head for all the data

--- seccion7/pages/_app.js
+++ seccion7/pages/_app.js
@@ -1,8 +1,13 @@
 import Layout from '../components/layout/layout'
 import '../styles/globals.css'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
   return <Layout>
+    <Head>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
     <Component {...pageProps} />
   </Layout>
 }
