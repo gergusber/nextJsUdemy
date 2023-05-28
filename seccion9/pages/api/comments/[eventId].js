94f56f8f231afe1e0a2570e6d47a6598a0be73ed
German Bertea <gebe@influencerindex.com>
2023-05-28 17:45:59 +0200

    fetch data from mongodb atlas

--- seccion9/pages/api/comments/[eventId].js
+++ seccion9/pages/api/comments/[eventId].js
@@ -1,8 +1,16 @@
 // get incomming requests as post and also fetch requests
-function handler(req, res) {
+const { MongoClient } = require("mongodb");
+const MONGODB_USER = process.env.MONGODB_USER;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+const MONGODB_DB = process.env.MONGODB_DB;
+
+const handler = async (req, res) => {
   // const email = req.query.email;
   const eventId = req.query.eventId
   console.log('Get request to backend');
+  console.log(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@clusterfrance.4ryh4fh.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority`)
+  const client = await MongoClient.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@clusterfrance.4ryh4fh.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority`); // creates the client 
+
   if (req.method === 'POST') {
 
     const { email, name, text } = req.body;
@@ -11,24 +19,30 @@ function handler(req, res) {
       res.status(422).json({ message: 'Bad request, email not valid' });
       return;
     }
-  
+
     if (!name || !name.trim() === '') {
       res.status(422).json({ message: 'Bad request, Name not valid' });
       return;
     }
-  
+
     if (!text || !text.trim() === '') {
       res.status(422).json({ message: 'Bad request, Text not valid' });
       return;
     }
-  
+
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
-      text
+      text,
+      eventId
     }
-  
+
+    console.log('Email in the api : ', email);
+
+    const db = client.db();
+
+    const result = await db.collection('comments').insertOne(newComment) // inserts an email to the emails collections,.
+    newComment.id = result.insertedId;
     console.log('Comment in the api : ', newComment);
 
     res.status(200).json({ message: 'Added Comment', comment: newComment })
@@ -36,15 +50,14 @@ function handler(req, res) {
 
 
   if (req.method === 'GET') {
-    console.log('Get request to backend');
-    const dummyList = [
-      { id: 'c1', name: 'German', text: 'something awesome' },
-      { id: 'c2', name: 'Maax', text: 'something awesome 2' }
-    ]
-    console.log('Events sent to: event', eventId, dummyList);
-    
-    res.status(200).json({ comments: dummyList })
+
+    const db = client.db(); 
+    const result = await db.collection('comments').find().sort({ _id: -1 }).toArray(); 
+
+    res.status(200).json({ comments: result })
   }
+
+  await client.close();
 }
 
 export default handler;
--- seccion9/pages/api/newsletter/index.js
+++ seccion9/pages/api/newsletter/index.js
@@ -1,20 +1,28 @@
-//sign up email handler for register.
+const { MongoClient } = require("mongodb");
+const MONGODB_USER = process.env.MONGODB_USER;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+const MONGODB_DB = process.env.MONGODB_DB;
 
-function handler(req, res) {
-  // const email = req.query.email;
+
+const handler = async (req, res) => {
   const { email } = req.body;
-
   if (req.method === 'POST') {
-
     if (!email || !email.includes('@')) {
       res.status(422).json({ message: 'Bad request' });
       return;
     }
 
     console.log('Email in the api : ', email);
+    const client = await MongoClient.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@clusterfrance.4ryh4fh.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority`); // creates the client 
 
+    const db = client.db();
+
+    const result = await db.collection('newsletter').insertOne({ email: email }) // inserts an email to the emails collections,.
+
+    console.log(`${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`);
 
     res.status(201).json({ message: 'sign up successfully ' })
+    await client.close();
   }
   else {
 
