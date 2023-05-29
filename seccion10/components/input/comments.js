df054668dc5301b7498c348a0c4275ba9d0eb1e5
German Bertea <gebe@influencerindex.com>
2023-05-29 17:26:52 +0200

    implement login and the context notifications

--- seccion10/components/input/comments.js
+++ seccion10/components/input/comments.js
@@ -1,20 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '../../store/notification-context'
+
 
 function Comments(props) {
   const { eventId } = props;
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
 
+  const context = useContext(NotificationContext);
+  const [isFetching, setIsFetching] = useState(false)
+
   useEffect(() => {
     if(showComments){
-
+      setIsFetching(true)
       console.log('Sending request to backend');
       fetch(`/api/comments/${eventId}`)
         .then(response => response.json())
-        .then(data => { setComments(data.comments) })
+        .then(data => {
+          setComments(data.comments)
+          setIsFetching(false)
+        })
     } 
   }, [showComments])
 
@@ -25,6 +33,12 @@ function Comments(props) {
   function addCommentHandler(commentData) {
     // send data to API
     const { email, name, text } = commentData;
+    
+    context.showNotification({
+      title: 'Adding comment',
+      message: 'Creating comment',
+      status: 'pending'
+    })
 
     fetch(`/api/comments/${eventId}`, {
       method: 'post',
@@ -32,8 +46,34 @@ function Comments(props) {
       headers: {
         'Content-Type': 'application/json'
       }
-    }).then(response => response.json())
-      .then(data => console.log(data))
+    }).then(response => {
+      if (response.ok) {
+        response.json()
+      }
+      else {
+        response.json().then(data => {
+          context.showNotification({
+            title: 'Error',
+            message: data.message || 'Registering to Newsletter failed',
+            status: 'error'
+          })
+        })
+      }
+      })
+      .then(data => {
+        context.showNotification({
+          title: 'Success!',
+          message: 'Registering to comment correct',
+          status: 'success'
+        })
+      })
+      .catch(err => {
+        context.showNotification({
+          title: 'signing up failed..',
+          message: err.message || 'Registering to Newsletter failed',
+          status: 'error'
+        })
+      })
   }
 
   return (
@@ -42,7 +82,9 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !isFetching && <CommentList items={comments} />}
+      {showComments && isFetching && <p>Loading...</p>}
+      
     </section>
   );
 }
