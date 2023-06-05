8199217a423f1c63cac39486673d0cb0243d6fdf
German Bertea <gebe@influencerindex.com>
2023-06-05 22:09:47 +0200

    Add portal for render the notifications

--- seccion11/components/layout/ui/notification.js
+++ seccion11/components/layout/ui/notification.js
@@ -17,12 +17,10 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
-    <div className={cssClasses}>
-      <h2>{title}</h2>
-      <p>{message}</p>
-    </div>
-  );
+  return ReactDOM.createPortal( (<div className={cssClasses}>
+    <h2>{title}</h2>
+    <p>{message}</p>
+  </div>),document.getElementById('notifications'));
 }
 
 export default Notification;
--- seccion11/pages/_document.js
+++ seccion11/pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div id="notifications" ></div>
         </body>
       </Html>)
   }
