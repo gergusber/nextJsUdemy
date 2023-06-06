268b23ad9ef4285a436d20726168aed594163675
German Bertea <gebe@influencerindex.com>
2023-06-06 10:58:01 +0200

    work with env and create export with seccion 12

--- seccion11/components/posts/post-detail/post-content.js
+++ seccion11/components/posts/post-detail/post-content.js
@@ -2,9 +2,15 @@ import classes from './post-content.module.css'
 import PostHeader from './post-header'
 import ReactMarkdown from 'react-markdown'
 import Image from 'next/image'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark, vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import vscDarkPlus from 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
 
+
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('css', css);
 
 const PostContent = (props) => {
   const { post } = props
