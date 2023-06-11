20603ee1f8f079dd011a3a6514c6a301e3f87a77
German Bertea <gerbertea@gmail.com>
2023-06-11 11:31:28 +0200

    a more elegant way to check the session on profile, directly working with getServersideProps

--- seccion13/components/profile/user-profile.js
+++ seccion13/components/profile/user-profile.js
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-import { useSession, getSession } from 'next-auth/client';
+// import { useSession, getSession } from 'next-auth/client';
 function UserProfile() {
+  // const [user, setUser] = useState(null);
 
   // const [session, loading] = useSession();
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getSession().then(
-      session => {
-        if (!session) {
-          window.location.href = '/auth'
-        }
-        else {
-          setIsLoading(false);
-        }
-        // setLoadedSession(session);
-      }
-    )
-  })
-  // const [user, setUser] = useState(null);
+  // useEffect(() => {
+  //   getSession().then(
+  //     session => {
+  //       if (!session) {
+  //         window.location.href = '/auth'
+  //       }
+  //       else {
+  //         setIsLoading(false);
+  //       }
+  //       // setLoadedSession(session);
+  //     }
+  //   )
+  // })
 
   if (isLoading) {
     return <p className={classes.profile}>Loading..</p>
--- seccion13/pages/profile.js
+++ seccion13/pages/profile.js
@@ -1,7 +1,29 @@
 import UserProfile from '../components/profile/user-profile';
+import { getSession } from 'next-auth/client';
 
 function ProfilePage() {
   return <UserProfile />;
 }
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req })
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    };
+  }
+
+
+  return {
+    props: {
+      session
+    }
+  }
+}
+
 
 export default ProfilePage;
