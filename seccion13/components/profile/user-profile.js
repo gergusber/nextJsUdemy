50370c1fc2ced4271be231211d22ffd9b47ca7b4
German Bertea <gerbertea@gmail.com>
2023-06-11 11:24:22 +0200

    check session on userprofile

--- seccion13/components/profile/user-profile.js
+++ seccion13/components/profile/user-profile.js
@@ -1,9 +1,30 @@
+import { useState, useEffect } from 'react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-
+import { useSession, getSession } from 'next-auth/client';
 function UserProfile() {
-  // Redirect away if NOT auth
 
+  // const [session, loading] = useSession();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then(
+      session => {
+        if (!session) {
+          window.location.href = '/auth'
+        }
+        else {
+          setIsLoading(false);
+        }
+        // setLoadedSession(session);
+      }
+    )
+  })
+  // const [user, setUser] = useState(null);
+
+  if (isLoading) {
+    return <p className={classes.profile}>Loading..</p>
+  }
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
